a87de09e3d58907ad7a165c0ad1720dd5825e92c
tootsi91 <tootsiats@gmail.com>
2017-03-29 22:53:55 +0300

    third commit

--- scraper_test.rb
+++ scraper_test.rb
@@ -4,10 +4,16 @@ require 'open-uri'
    
 page = Nokogiri::HTML(open("http://www.err.ee/uudised"))   
 
-links = page.css("a")
-#times = page.css("div.time-font_news-time")
-#puts times
-puts links.text
-puts links[0]["href"]
+links = page.css("div.news-row a");
+time = page.css("div.time-font.news-time")
+#links = page.css("div.container div.real-fp-container div.right-block div.list-item-node.mb52.r-blocks.automatic-blockframe div.block-frame.block-frame-default.margin-button.article-margin div.container-element.no-margin div.block-frame div.table-list-row.itemlist div.table-list-cell.news-list-item.article div.news_row a")
 
-#page.css('div_class#"time-font news-time"')
+#time = page.css("div.container div.real-fp-container div.right-block div.list-item-node.mb52.r-blocks.automatic-blockframe div.block-frame.block-frame-default.margin-button.article-margin div.container-element.no-margin div.block-frame div.table-list-row.itemlist div.table-list-cell.news-list-item.article div.news_row div.time-font.news-time")
+
+
+#links = page.css("a")
+puts time[0].text
+puts links[0].text
+puts links[0]["href"]
+#puts time.text
+#puts time[0]
